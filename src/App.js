f33007804c3904d25629072e80efe2f1d4ac9358
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-01-30 20:40:52 +1000

    add rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project has been coded by Susanna Mjetk and is{" "}
-        <a href="https://silver-kelpie-c76d5e.netlify.app" target="_blank">
+        <a
+          href="https://silver-kelpie-c76d5e.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
