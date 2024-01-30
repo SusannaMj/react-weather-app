912f1b1c4f860641a885ebe9560851a341a237c6
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-01-30 21:19:05 +1000

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project has been coded by Susanna Mjetk and is{" "}
-        <a
-          href="https://silver-kelpie-c76d5e.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project has been coded by Susanna Mjetk and is{" "}
+          <a
+            href="https://silver-kelpie-c76d5e.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
