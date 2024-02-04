035ebd880c643a1cba1e0c70ed0307644e5443e8
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-02-04 23:27:55 +1000

    added axios and weather API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Spain" />
 
         <footer>
           This project has been coded by Susanna Mjetk and is{" "}
