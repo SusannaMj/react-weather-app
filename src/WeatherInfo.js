4d23318560d8a92e8bbdb70d4c72c889be525cf3
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-02-12 23:28:49 +1000

    added WeatherTemperature component for unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -17,10 +18,7 @@ export default function WeatherInfo(props) {
           </div>
           <div className="col-6">
             <WeatherIcon alt={props.info.description} code={props.info.icon} />
-            <span className="temperature">
-              {Math.round(props.info.temperature)}
-            </span>
-            <span className="unit">°C</span>
+            <WeatherTemperature celsius={props.info.temperature} />
           </div>
         </div>
       </div>
