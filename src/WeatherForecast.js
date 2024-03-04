fc9bf0814ab80a7aadee5cd58bc868b44a2ae283
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-03-04 22:14:20 +1000

    use useEffect to update forecast data when city/coordinates change

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -7,6 +7,11 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function showForecast(response) {
     console.log(response.data);
     setLoaded(true);
@@ -19,7 +24,7 @@ export default function WeatherForecast(props) {
       <div className="weatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index % 8 == 0) {
+            if (index % 8 === 0) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay forecastData={dailyForecast} />
